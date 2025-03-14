2c8c46b4a4a3e9c507d2a323e7bfb39519139be4
Lucifer220228 <samy25502812@gmail.com>
2025-03-15 00:18:39 +0800

    rewrite App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Student } from "./interface/Student";
 import { Seat } from "./interface/Seat";
-import { Timeslot } from "./interface/Timeslot";
+import { Timeslot } from "./interface/Timeslots";
 import { Reservation } from "./interface/Reservation";
 
 export default function App() {
